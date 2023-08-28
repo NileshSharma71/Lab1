4d629ed744864689d266f74ab5dc4b65b7a7f252
Nilesh Sharma <sharmanilesh7105@gmail.com>
2023-08-28 09:40:22 +0530

    added first file

---
+++ question1.py
@@ -0,0 +1,5 @@
+x=int(input("Enter value of one parallel side(value of x): "))
+y=int(input("Enter value of second parallel side(value of y): "))
+h=int(input("Enter value of height of trapezoid(value of h): "))
+area= (1/2)*(x+y)*h
+print(area)
