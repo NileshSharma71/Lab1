859bbdb65e58c4951f07d16f62965a0ad4cf306d
Nilesh Sharma <sharmanilesh7105@gmail.com>
2023-09-10 19:16:28 +0530

    completed the work

---
+++ question6.py
@@ -0,0 +1,9 @@
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+print("Before swapping:")
+print("Value of a is:",a,"and value of b is:",b)
+a=a*b
+b=a/b
+a=a/b
+print("After swapping:")
+print("Value of a is:",a,"and value of b is:",b)
